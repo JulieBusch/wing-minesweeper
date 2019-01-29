d34054d455db2da692ff8b288c1afcd5b324d0c1
Julie Busch <heyjuliebusch@gmail.com>
2019-01-29 18:00:47 -0500

    need to check neighbors of safe cells

--- game.js
+++ game.js
@@ -1,18 +1,28 @@
 const minesweeper = {
   createAndShowBoard: function (height, width) {
-    console.log('AAAHHHHHHHHHHHH');
     this.height = height;
     this.width = width;
 
+    function bombPlanter() {
+      const num = Math.ceil(Math.random() * 10);
+      if ((height === 10 && num >= 9)
+        || (height === 20 && num >= 8)
+        || (height === 30 && num >= 7)) {
+          return 'true';
+      }
+
+      return 'false';
+    }
+
     // create <table> element
-    const table = document.createElement("tbody");
+    const table = document.getElementsByTagName("tbody")[0] || document.createElement("tbody");
 
     // build Table HTML
     let tablehtml = '';
     for (let h = 0; h < height; h++) {
       tablehtml += `<tr id="row${h}>`;
       for (let w = 0; w < width; w++) {
-        tablehtml += `<td data-status='dead' id="${w}-${h}" class="hidden"></td>`;
+        tablehtml += `<td mine="${bombPlanter()}" id="${w}-${h}" class="hidden"></td>`;
       }
       tablehtml += "</tr>";
     }
@@ -35,15 +45,6 @@ const minesweeper = {
   },
 
   setupBoardEvents: function() {
-    function explode() {
-      this.forEachCell(function(cell) {
-        if (cell.getAttribute('mine') === 'true') {
-          cell.className = 'mine';
-        } else {
-          cell.className = 'revealed';
-        }
-      });
-    }
 
     function getNeighbors(cell) {
       const dash = cell.getAttribute("id").indexOf("-");
@@ -74,11 +75,22 @@ const minesweeper = {
       return neighborsSelector;
     }
 
+    const explode = (clickedCell) => {
+      this.forEachCell(function(cell) {
+        if (cell.getAttribute('mine') === 'true') {
+          cell.className = 'mine';
+        } else {
+          cell.className = 'revealed';
+        }
+      });
+    }
+
     function reveal(cell) {
       const neighbors = getNeighbors(cell);
-      let count = 0;
+      const count = 0;
 
       neighbors.forEach(function(neighbor){
+        console.log(count);
         const neighborCell = document.getElementById(neighbor);
         if (neighborCell.getAttribute('mine') === 'true') {
           count++;
@@ -86,81 +98,46 @@ const minesweeper = {
       });
 
       if (count) {
-        cell.innerText(`${count}`);
+        cell.innerHTML(`${count}`);
       }
     }
 
     function onCellClick(e) {
+
+      function victoryCheck() {
+        const unclickedBombs = 0;
+        const clickedSafeSquares = 0;
+        this.forEachCell(function(cell) {
+          if (cell.getAttribute('clicked') === 'false' && cell.getAttribute('mine') === 'true') {
+            unclickedBombs++;
+          };
+          if (cell.getAttribute('clicked') === 'true' && cell.getAttribute('mine') === 'false') {
+            clickedSafeSquares++;
+          }
+        });
+    
+        if ((unclickedBombs + clickedSafeSquares) === (this.height * this.width)) {
+          document.getElementsByTagName('body').appendChild('<div class="victory">You Win!</div>')
+        };
+      }
+
+      this.setAttribute('clicked', 'true');
       if (this.getAttribute('mine') === 'true') {
         this.className = 'mine';
-        explode();
+        explode(this);
       } else {
         this.className = 'safe';
         reveal(this);
-      }
+        victoryCheck();
+        
+      };
     };
 
     this.forEachCell(function(cell) {
       cell.onclick = onCellClick;
     });
-  },
-
-  step: function () {
-    const allCells = document.getElementsByTagName("td");
-    const cellArray = Array.from(allCells);
-    cellArray.forEach(function(cell){
-
-      const dash = cell.getAttribute("id").indexOf("-");
-      const x = parseInt(cell.getAttribute("id").slice(0, dash), 10);
-      const y = parseInt(cell.getAttribute("id").slice(dash + 1), 10);
-
-      const neighborArray = [
-        {x : x-1,   y : y-1},
-        {x : x,     y : y-1},
-        {x : x+1,   y : y-1},
-        {x : x-1,   y : y},
-        {x : x+1,   y : y},
-        {x : x-1,   y : y+1},
-        {x : x,     y : y+1},
-        {x : x+1,   y : y+1}
-      ];
-      const neighborsSelector = [];
-
-      neighborArray.forEach(function(coord){
-        if ((0 <= coord.x) &&
-            (coord.x < 12) &&
-            (0 <= coord.y) &&
-            (coord.y < 12) ){
-          console.log(coord.x, coord.y);
-          neighborsSelector.push(`${coord.x}-${coord.y}`);
-        }
-      });
-
-      const count = 0;
-
-      neighborsSelector.forEach(function(neighbor){
-        const neighborCell = document.getElementById(neighbor);
-        if (neighborCell.getAttribute('mine') === true){
-          count++;
-        }
-      });
-      
-    });
-
-    cellArray.forEach(function(cell){
-      if (cell.getAttribute("title")==="dead") {
-        cell.className = 'dead';
-        cell.setAttribute('data-status', 'dead');
-      } else if (cell.getAttribute("title")==="alive") {
-        cell.className = 'alive';
-        cell.setAttribute('data-status', 'alive');
-      }
-    });
-  
-  
-  
-    }
-  };
+  }
+};
 
 const easyGameButton = document.getElementById('start-easy-btn');
 easyGameButton.onclick = () => { minesweeper.createAndShowBoard(10, 10); };
